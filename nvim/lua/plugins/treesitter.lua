c97dadd3cebc82321a06fa30fc26a04e2971a030
antoniomayk <antoniomayk.g@gmail.com>
2024-08-04 11:14:49 -0300

    feat: improve treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,16 @@
 return {
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "java", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = true,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+  },
 }
